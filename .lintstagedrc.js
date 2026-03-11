b1d67dc540fdb62b6d692caaa9bf972f266e07de
Brain <bagir89@gmail.com>
2026-03-11 13:40:02 +0800

    "feat:eslint-prettier-configured

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
-module.exports = { 
-  '*.{ts,tsx}': ['eslint --fix', 'prettier --write'], 
-  '*.{js,jsx}': ['eslint --fix', 'prettier --write'], 
-  '*.{json,md}': ['prettier --write'], 
+module.exports = {
+  "*.{ts,tsx}": ["eslint --fix", "prettier --write"],
+  "*.{js,jsx}": ["eslint --fix", "prettier --write"],
+  "*.{json,md}": ["prettier --write"],
 };
